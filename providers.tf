fd30d01893c48713cbc489f9ee4a4dc33cf58a50
root <harpreetkhaira@hotmail.com>
2026-06-14 20:58:04 +0530

    fourt commit

--- providers.tf
+++ providers.tf
@@ -1,20 +1,11 @@
 terraform {
-  required_version = ">=1.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~>5.0"
-    }
-  }
-
-  backend "s3" {
-    bucket = "harpreet-terraform-state-001"
-    key    = "eks/terraform.tfstate"
-    region = "us-east-1"
-  }
+backend "s3" {
+bucket = "harpreet-terraform-state-001"
+key    = "eks/terraform.tfstate"
+region = "ap-south-1"
+}
 }
 
 provider "aws" {
-  region = var.aws_region
+region = "ap-south-1"
 }
