0fff49664a292f7aa8c3c838c6d76716c590755f
root <harpreetkhaira@hotmail.com>
2026-06-15 15:02:56 +0530

    Fifth-actions

--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
 terraform {
-backend "s3" {
-bucket = "harpreet-terraform-state-001"
-key    = "eks/terraform.tfstate"
-region = "ap-south-1"
-}
+  backend "s3" {
+    bucket = "harpreet-terraform-state-001"
+    key    = "eks/terraform.tfstate"
+    region = "ap-south-1"
+  }
 }
 
 provider "aws" {
-region = "ap-south-1"
+  region = "ap-south-1"
 }
