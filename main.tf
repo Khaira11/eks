5f3a04882ee81c292233f94354c4a3701b588ce8
root <harpreetkhaira@hotmail.com>
2026-06-16 14:40:30 +0530

    16 commit

--- main.tf
+++ main.tf
@@ -1,28 +1,45 @@
 # Filter out available Availability Zones
+
+# 1. Create VPC for EKS
 data "aws_availability_zones" "available" {}
 
-# 1. Create a Virtual Private Cloud (VPC) for the EKS Cluster
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
+source  = "terraform-aws-modules/vpc/aws"
+version = "~> 5.21"
 
-  name = "eks-vpc"
-  cidr = "10.0.0.0/16"
+name = "eks-vpc"
+cidr = "10.0.0.0/16"
 
-  azs             = slice(data.aws_availability_zones.available.names, 0, 3)
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+azs = slice(
+data.aws_availability_zones.available.names,
+0,
+3
+)
 
-  enable_nat_gateway = true
-  single_nat_gateway = true # Keeps costs lower for beginners
+public_subnets = [
+"10.0.101.0/24",
+"10.0.102.0/24",
+"10.0.103.0/24"
+]
 
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
+private_subnets = [
+"10.0.1.0/24",
+"10.0.2.0/24",
+"10.0.3.0/24"
+]
 
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
-  }
+enable_nat_gateway = true
+single_nat_gateway = true
+
+map_public_ip_on_launch = true
+
+public_subnet_tags = {
+"kubernetes.io/role/elb" = 1
+}
+
+private_subnet_tags = {
+"kubernetes.io/role/internal-elb" = 1
+}
 }
 
 # 2. Create the EKS Cluster
